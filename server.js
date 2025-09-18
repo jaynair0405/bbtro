cf107fcb932b52416fb08f6e0447d2eab0797bb1
jaynair0405 <111112158+jaynair0405@users.noreply.github.com>
2025-09-18 12:43:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ const upload = multer({ dest: "uploads/" });
 // MySQL connection pool
 // const pool = mysql.createPool({
 //   host: "localhost",
-//   user: "jay",           // change as per your user
-//   password: "4310jay",  // change as per your password
+//   user: "0000",           // change as per your user
+//   password: "888888",  // change as per your password
 //   database: "bbtro",
 //   connectionLimit: 10,   // Maximum number of connections in pool
 //   queueLimit: 0,         // Maximum number of connection requests in queue (0 = unlimited)
